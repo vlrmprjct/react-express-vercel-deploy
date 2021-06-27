c76b433d6a8071b2dadf69b135c0b4aa7eb80266
Thomas M <mail@lokalnetz.com>
2021-06-27 10:55:39 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+const AnotherPage = () => <h1>Another Page</h1>;
+const NotFound = () => <h1>404 Not Found</h1>;
+class Home extends Component {
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    const response = await fetch('/api/world', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ post: this.state.post }),
+    });
+    const body = await response.text();
+
+    this.setState({ responseToPost: body });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/another-page/">Another Page</Link>
+            </li>
+          </ul>
+        </nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.response}</p>
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <strong>Post to Server:</strong>
+            </p>
+            <input
+              type="text"
+              value={this.state.post}
+              onChange={e => this.setState({ post: e.target.value })}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.responseToPost}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+const App = () => (
+  <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/another-page/" component={AnotherPage} />
+        <Route component={NotFound} />
+      </Switch>
+  </Router>
+);
+
+export default App;
